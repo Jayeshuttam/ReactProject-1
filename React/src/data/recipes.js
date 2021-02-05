e0eb637899f6413f5376d298e0647da73e217d60
ftoumi <ftoumi@gmail.com>
2021-02-05 09:48:43 +0100

    pass params to component and retrieve data

---
+++ React/src/data/recipes.js
@@ -0,0 +1,36 @@
+export const recipesData = [
+  {
+    "name": "couscous",
+    "image": require('../images/chief.jpg'),
+    "ingredients": [
+      {
+        "name": "semoule",
+        "quantity": "1",
+        "unit": "kg"
+      },
+      {
+        "name": "courgettes",
+        "quantity": "1",
+        "unit": "g"
+      },
+
+    ]
+  },
+  {
+    "name": "Thé à la menthe",
+    "image": require('../images/chief.jpg'),
+    "ingredients": [
+      {
+        "name": "menthe",
+        "quantity": "1",
+        "unit": "kg"
+      },
+      {
+        "name": "sucre",
+        "quantity": "1",
+        "unit": "g"
+      },
+
+    ]
+  }
+]
--- React/src/screens/RecipeDetails.js
+++ React/src/screens/RecipeDetails.js
@@ -1,18 +1,23 @@
 import React from 'react'
+import { render } from 'react-dom'
 import {View,Text,Image,ImageBackground,TouchableOpacity,} from 'react-native'
 import { Modalize } from 'react-native-modalize'
 import Titre from './Titre'
+import {recipesData} from '../data/recipes'
 
 
-export default function RecipeDetails({route}){
-       const {id}=route.params
- return(
+export default class Recipe extends React.Component{
+    
+    render() {
+        console.log(recipesData)
 
-<View>
-    <Text>HEllo {id} </Text>
-</View>
-
- );
-
+        const {id} = this.props.navigation.state.params
+        const recipe = recipesData[id]
+        return(
+            <View>
+                <Text>{recipe.name}</Text>
+            </View>
+            );
+    }
 
 }
